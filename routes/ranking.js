4f44bcbd65b61fc875526e5929245c9db5b0ae60
frics <jimmy9711@naver.com>
2021-04-20 09:31:54 +0000

    swagger api doc 구현중

--- routes/ranking.js
+++ routes/ranking.js
@@ -7,12 +7,12 @@ const router = express.Router();
 router.get('/list', async(req, res, next) => {
   console.log("랭킹 리스트 전송");
   var result = new Object();
-  var script = req.query.script;
+  var script_id = req.query.id;
 
   try{
     const rankingList = await Ranking.findAll({
-        where: {script: script},
-        attributes:['script', 'nickname', 'score', 'date'],
+        where: {script_id: script_id},
+        attributes:['nickname', 'score', 'date'],
         order: [['score', 'DESC']],
         raw: true,
     });
